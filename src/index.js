9a916b5b08a31fd8cbd962e5e3c00472c7fc86c0
Victor <you@example.com>
2021-04-10 23:48:42 +0300

    fix index

--- src/index.js
+++ src/index.js
@@ -1,1 +1,61 @@
-import './styles.css';
+import './style.css';
+import Pixabay from './js/api';
+import template from './templates/templates.hbs';
+import { onOpenModal } from './js/modal';
+
+import '../node_modules/material-design-icons'
+
+const refs = getRefs();
+const pixabayService = new Pixabay();
+
+refs.searchForm.addEventListener('submit', onSearch);
+refs.gallery.addEventListener('click', onOpenModal);
+
+function onSearch(event) {
+    event.preventDefault();
+
+    pixabayService.query = event.currentTarget.elements.query.value;
+
+    if (pixabayService.query === '') {
+    return alert('Enter the word');
+  }
+
+    pixabayService.resetPage();
+    clearHits();
+    pixabayService.fetchHits().then(hits => {
+        appendHitsMarkup(hits);
+        pixabayService.incrementPage();
+    });
+}
+
+function appendHitsMarkup(hits) {
+    refs.gallery.insertAdjacentHTML('beforeend', template(hits));
+}
+
+function clearHits(){
+    refs.gallery.innerHTML = '';
+}
+
+function getRefs() {
+    return {
+        searchForm: document.querySelector('.search-form'),
+        gallery: document.querySelector('.gallery'),
+        scroll: document.querySelector('#scroll'),
+    };
+}
+
+const onEntry = entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting && pixabayService.query !== '') {
+            pixabayService.fetchHits().then(hits => {
+                appendHitsMarkup(hits);
+                pixabayService.incrementPage();
+            });
+        }
+    });
+};
+
+const observer = new IntersectionObserver(onEntry, {
+  rootMargin: '150px',
+});
+observer.observe(refs.scroll);
